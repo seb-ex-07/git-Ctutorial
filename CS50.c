599cea52393d68920e3c345b544846f5e8a5f05f
Sebastian Daniel <sebastiandaniel0709@gmail.com>
2026-08-31 21:04:17 +0800

    quickchange

--- CS50.c
+++ CS50.c
@@ -9,7 +9,7 @@ int main(void)
     printf("How old are you?\n");
     scanf ("%i", &age);
     printf("WOW YOU ARE %i years old!\n", age);
-    printf ("MAS GUMANDA KA NG %i years moree!\n", age);
+    printf ("LIVE!!! %i years moree!\n", age);
 
     return 0;
 }
