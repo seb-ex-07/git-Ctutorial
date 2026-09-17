a65fac8d6e6527c637ba2577216637fd07a82ad2
Sebastian Daniel <sebastiandaniel0709@gmail.com>
2026-09-17 14:27:15 +0800

    Second Converter

---
+++ Practices/secconv.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+/* SECOND CONVERTER*/
+
+/*Welcome to my second converter, where the function of this program is to convert 
+any wanted seconds into hours, minutes, and seconds*/
+
+/*this program highlights the use of modulo (%) and division (/)
+for it to work, also I'll add some creativity rather than just some input and output for seconds*/
+
+/*IPO
+
+INPUT: any number of seconds
+PROCESS: turn the seconds to hours, minutes, and seconds
+OUTPUT: display the hours, minutes, and seconds
+*/
+
+
+int main (void)
+{
+
+    int totalSeconds;
+
+    printf ("WELCOME TO SECOND CONVERTER!\n");
+    printf ("Please enter the seconds you want to conveter: \n");
+    scanf ("%d", &totalSeconds);
+    
+    int hrs = totalSeconds / 3600;
+    int rmngSeconds = totalSeconds % 3600;
+    int min = rmngSeconds / 60;
+    int sec = rmngSeconds % 60;
+    
+
+    printf ("THE CONVERTED SECONDS: \n");
+    printf ("HOURS: %d\n", hrs);
+    printf ("MINUTES: %d\n", min);
+    printf ("SECONDS: %d\n", sec);
+
+    printf ("That is %d hours, %d minutes and %d seconds!\n", hrs, min, sec);
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+}
